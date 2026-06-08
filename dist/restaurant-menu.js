ba5badbf3747988de608ac57767267fe98c392ad
marhano <carullomarjan@gmail.com>
2026-06-08 12:18:30 +0800

    feat: enhance compact layout for table info, search, filter, and item cards in CSS

--- dist/restaurant-menu.js
+++ dist/restaurant-menu.js
@@ -1,7 +1,7 @@
 /*!
  * restaurant-menu.js v0.0.1
  * Restaurant Menu & Basket Library
- * Built: 2026-06-08T03:44:04.996Z
+ * Built: 2026-06-08T04:02:01.003Z
  * Requires: jQuery 3+
  * License: MIT
  */
